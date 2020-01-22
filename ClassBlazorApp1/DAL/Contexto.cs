33e14d643cdd37cce31c6bc58142f6e8455a9317
Emmanuel Suarez Taveras <manuel.413@hotmail.com>
2020-01-22 19:47:06 -0400

    Agregando la entidad personas y la clase contexto configurada

---
+++ ClassBlazorApp1/DAL/Contexto.cs
@@ -0,0 +1,22 @@
+﻿using ClassBlazorApp1.Entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassBlazorApp1.DAL
+{
+    public class Contexto : DbContext
+    {
+
+        public DbSet<Personas> Personas { get; set; }
+
+        
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server = .\SqlExpress; Database = ClassTestDb; Trusted_Connection = True; ");
+        }
+    }
+}
---
+++ ClassBlazorApp1/Entidades/Personas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassBlazorApp1.Entidades
+{
+    public class Personas
+    {
+        [Key]
+        public int PersonaId { get; set; }
+        public string Nombres { get; set; }
+        public int Edad { get; set; }
+
+
+        public Personas()
+        {
+            PersonaId = 0;
+            Nombres = string.Empty;
+        }
+    }
+    
+}
